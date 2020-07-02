1e1047875625e53021151e714ca5dcac222509d1
Jhonny Göransson <jhonny.goransson@gmail.com>
2020-07-02 12:07:32 +0200

    Add extension sources from private repo

---
+++ vulkan/src/vulkan.cpp
@@ -0,0 +1,30 @@
+// Defold SDK
+#define DLIB_LOG_DOMAIN LIB_NAME
+#include <dmsdk/sdk.h>
+
+#define LIB_NAME "Vulkan"
+
+extern "C" void GraphicsAdapterVulkan();
+
+dmExtension::Result AppInitializeVulkan(dmExtension::AppParams* params)
+{
+    GraphicsAdapterVulkan();
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result InitializeVulkan(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result AppFinalizeVulkan(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result FinalizeVulkan(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+DM_DECLARE_EXTENSION(Vulkan, LIB_NAME, AppInitializeVulkan, AppFinalizeVulkan, InitializeVulkan, 0, 0, FinalizeVulkan)
